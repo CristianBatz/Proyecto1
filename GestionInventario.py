582c6863a1631d76927e9ba01c01a83adeff88e9
CristianBatz <ce.sbatz@udeo.edu.gt>
2025-08-15 10:30:37 -0600

    se creo el menu con las opciones pedidas

--- GestionInventario.py
+++ GestionInventario.py
@@ -1,2 +1,31 @@
 class Producto:
     pass
+
+opcion = 0
+while opcion != 6:
+    print("=== Gestion Inventario ===")
+    print("1. Registrar producto")
+    print("2. Mostrar productos")
+    print("3. Buscar producto")
+    print("4. Actualizar producto")
+    print("5. Eliminar producto")
+    print("6. Salir")
+    try:
+        opcion = int(input("Seleccione una opcion: "))
+    except ValueError:
+        print("Opcion no valida")
+        continue
+
+    match opcion:
+        case 1:
+            pass
+        case 2:
+            pass
+        case 3:
+            pass
+        case 4:
+            pass
+        case 5:
+            pass
+        case 6:
+            print("Saliendo del programa")
